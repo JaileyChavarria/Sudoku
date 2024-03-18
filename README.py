72ad360751af5cfe6aef5fe8a023c18f310d4d81
JaileyChavarria <160268915+JaileyChavarria@users.noreply.github.com>
2024-03-17 23:28:52 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,13 @@
 # Sudoku 
+def print_grid(grid):
+  for row in grid:
+    print(" ".join(str(num) if num !=0 else "-" for num in row))
+
+def generate_blank_grid(size):
+  return[[0 for _ in range(size)] for _ in range(size)]
+
+if __name__ == "__main__":
+  size = 9 
+  sudoku_grid = generate_blank_grid(size)
+  print("Blank Sudoku Grid:")
+  print_grid(sudoku_grid)
